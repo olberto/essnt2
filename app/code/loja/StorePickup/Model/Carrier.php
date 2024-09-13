96a99f290078928375d2f14e28970d526a40a10f
Olberto Morais <olberto1@hotmail.com>
2024-09-13 16:08:57 -0300

    Create Carrier.php

---
+++ app/code/loja/StorePickup/Model/Carrier.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace YourVendor\StorePickup\Model;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+
+class Carrier extends AbstractCarrier implements CarrierInterface
+{
+    protected $_code = 'storepickup';
+
+    public function collectRates(RateRequest $request)
+    {
+        $result = $this->_rateResultFactory->create();
+        $method = $this->_rateMethodFactory->create();
+
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod($this->_code);
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $method->setPrice(0);  // Sem custo de envio
+        $method->setCost(0);
+
+        $result->append($method);
+        return $result;
+    }
+
+    public function getAllowedMethods()
+    {
+        return [$this->_code => $this->getConfigData('name')];
+    }
+}
